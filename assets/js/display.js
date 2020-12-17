e81a284c0a6b4aca4e02a093f2a2b2507e5cbfbc
unknown <mounikareddygonae15@gmail.com>
2020-12-17 18:24:39 +0530

    show search result & style table

--- assets/js/display.js
+++ assets/js/display.js
@@ -7,3 +7,35 @@
 // var x= `<%-JSON.stringify(results)%>`;
 // console.log(x , "xxx");
 
+// var searchBox = document.getElementById('search-box');
+// var tabledata = document.getElementsByTagName('td')
+// // console.log(tabledata[0].innerHTML, 'tabledata');
+// var displayData = document.getElementById('display-Data')
+// console.log(displayData.value, "display");
+// function searchData() {
+//     console.log(searchBox.value)
+//     let matches = tabledata[0].filter(hero => {
+//         //  regular expression is a way to  search 
+//         const regex = new RegExp(`^${searchBox.value}`, 'gi');
+//         return hero.match(regex);
+//      });
+//      console.log(matches, "matches")
+// }
+
+const searchData = () => {
+    var searchInput = document.getElementById('search-input').value.toUpperCase();
+    var tableData = document.getElementById('table-data');
+    var rowData  = tableData.getElementsByTagName('tr');
+    for(var i=0 ;i <rowData.length;i++) {
+        let td = rowData[i].getElementsByTagName('td')[2];
+        if(td) {
+          let txtValue = td.textContent || td.innerHTML;
+            if (txtValue.toUpperCase().indexOf(searchInput) > -1) {
+                rowData[i].style.display = "";
+            } else {
+                rowData[i].style.display = "none";
+            }
+          }
+        }
+    }
+
